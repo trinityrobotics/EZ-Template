539bb17bbc8e5234382f6f89f0c65087d11e1ba5
Jess Zarchi <jackfz01@gmail.com>
2022-01-13 15:46:37 -0800

    Fixed formatting

--- include/EZ-Template/Odometry/tracking_wheel.hpp
+++ include/EZ-Template/Odometry/tracking_wheel.hpp
@@ -1,8 +1,15 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #pragma once
+
 #include "main.h"
-class Tracking_Wheel
-{
-public:
+
+class Tracking_Wheel {
+ public:
   Tracking_Wheel(pros::Motor motor);
   Tracking_Wheel(pros::Rotation rotationSensor);
   Tracking_Wheel(pros::ADIEncoder encoder);
@@ -11,7 +18,8 @@ public:
   double offset;
   double ratio;
   double wheel_size;
-private:
+
+ private:
   std::function<double()> get_value_func;
   std::function<void()> reset_func;
 };
--- src/EZ-Template/Odometry/tracking_wheel.cpp
+++ src/EZ-Template/Odometry/tracking_wheel.cpp
@@ -1,26 +1,25 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #include "tracking_wheel.hpp"
-Tracking_Wheel::Tracking_Wheel(pros::Motor motor)
-{
+
+Tracking_Wheel::Tracking_Wheel(pros::Motor motor) {
   get_value_func = std::bind(&pros::Motor::get_position, motor);
   reset_func = std::bind(&pros::Motor::tare_position, motor);
 }
 
-Tracking_Wheel::Tracking_Wheel(pros::Rotation rotationSensor)
-{
+Tracking_Wheel::Tracking_Wheel(pros::Rotation rotationSensor) {
   get_value_func = std::bind(&pros::Rotation::get_position, rotationSensor);
   reset_func = std::bind(&pros::Rotation::reset_position, rotationSensor);
 }
-Tracking_Wheel::Tracking_Wheel(pros::ADIEncoder encoder)
-{
+
+Tracking_Wheel::Tracking_Wheel(pros::ADIEncoder encoder) {
   get_value_func = std::bind(&pros::ADIEncoder::get_value, encoder);
   reset_func = std::bind(&pros::ADIEncoder::reset, encoder);
 }
 
-double Tracking_Wheel::get_value()
-{
-  return get_value_func();
-}
-void Tracking_Wheel::reset_position()
-{
-  reset_func();
-}
+double Tracking_Wheel::get_value() { return get_value_func(); }
+void Tracking_Wheel::reset_position() { reset_func(); }
